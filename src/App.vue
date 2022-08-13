b125384a48662d035773d2834145744a24542f5d
Declan Tyson <declan.j.tyson@gmail.com>
2022-08-13 15:38:52 +0100

    ep41

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,8 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Hello there" />
-  </div>
+  <h1 class="mt-8 text-6xl text-center">Hello there</h1>
 </template>
-
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
